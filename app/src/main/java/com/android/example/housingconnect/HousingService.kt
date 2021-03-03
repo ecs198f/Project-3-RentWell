15776a2f130db044c6993dfe95260cdce95ba8a0
OmarBurney <46209616+OmarBurney@users.noreply.github.com>
2021-03-02 16:01:48 -0800

    Fix HousingService Phase Typo (3.3 to 2.3)

--- app/src/main/java/com/android/example/housingconnect/HousingService.kt
+++ app/src/main/java/com/android/example/housingconnect/HousingService.kt
@@ -1,15 +1,15 @@
 package com.android.example.housingconnect
 
 interface HousingService {
-    // TODO: PHASE 3.3 - Define GET "housing/all" endpoint
+    // TODO: PHASE 2.3 - Define GET "housing/all" endpoint
     //      returns a list of Posts
 
 
-    // TODO: PHASE 3.3 - Define POST "housing" endpoint
+    // TODO: PHASE 2.3 - Define POST "housing" endpoint
     //      returns a Message
 
 
-    // TODO: PHASE 3.3 - Define POST "images/upload" endpoint
+    // TODO: PHASE 2.3 - Define POST "images/upload" endpoint
     //      returns an ImageUploadResponse
 
     // NOTE: Don't need to define "/images/{id}" endpoint, Glide will handle that for us
